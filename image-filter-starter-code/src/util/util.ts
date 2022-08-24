3ed44bc97bfa843253e4cbb67a2e65b37a751653
Christian Ojiezele <christianojiezele@gmail.com>
2022-08-24 14:59:11 +0100

    completed the filtered image endpoint and its working as expected

--- image-filter-starter-code/src/util/util.ts
+++ image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,8 @@
 import fs from "fs";
-import Jimp = require("jimp");
+import Jimp from 'jimp';
+// import Jimp = require("jimp");
+
+// var Jimp = require("jimp");
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -11,7 +14,9 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
+
       const photo = await Jimp.read(inputURL);
+      
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
